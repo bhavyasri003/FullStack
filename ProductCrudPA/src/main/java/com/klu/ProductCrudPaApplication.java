aa9d0faec1920c5feced8986bb73fc18d188f4c2
bhavyasri003 <2420030181@klh.edu.in>
2026-03-04 13:44:19 +0530

    Add files via upload

---
+++ ProductCrudPA/src/main/java/com/klu/ProductCrudPaApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudPaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCrudPaApplication.class, args);
+	}
+
+}
